5aac9f8d99cfc62ede3f7d38d35b3ce597bbcd31
CloudlessJuan <jpablovasquezarenas@gmail.com>
2024-09-18 00:39:00 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,186 @@
+from dash import Dash, dcc, html, callback_context, Output, Input
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
+from sql_utilities import sql_queries
+
+#Dash App
+dash_app = Dash(external_stylesheets=[dbc.themes.SLATE])
+app = dash_app.server
+
+#Configuración de estilos
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "20%",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+    "overflow-y": "scroll",
+}
+
+CONTENT_STYLE = {
+    "margin-left": "10%",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa"
+}
+
+LANGUAGE_BAR_STYLE = {
+    "position": "fixed",
+    "top": "10px",
+    "right": "1px",
+    "padding": "10px",
+    "background-color": "#f8f9fa",
+    "border": "1px solid #dee2e6",
+    "border-radius": "5px",
+    "box-shadow": "0 2px 5px rgba(0, 0, 0, 0.1)"
+}
+
+
+
+#Contenido Eng/Esp
+content_text = {
+    'es': {
+        'title': 'Monitor de precios de arrendamiento del Valle de Aburrá',
+        'sidebar_title': 'Datos técnicos',
+        'description': """
+        Este proyecto tiene como objetivo proporcionar datos de referencia para analizar las dinámicas del mercado inmobiliario en la región
+        utilizando técnicas de webscraping de sitios web de anuncios inmobiliarios. El objetivo es identificar el comportamiento de la oferta
+        y precios de los arrendamientos en el Valle de Aburrá. Los datos recopilados corresponden a anuncios para apartamentos con al menos
+        dos habitaciones y dos baños, utilizando un proceso automatizado que emplea los servicios gratuitos de Azure.
+
+        Los servicios utilizados en este proyecto son:
+
+        - **Azure Functions**: Webscraping automatizado. [Enlace](https://github.com/Azure/azure-functions)
+        - **Azure DataLake Gen2**: Almacenamiento de datos recopilados mediante webscraping.
+        - **Azure Data Factory**: Ingesta de datos crudos a un db SQL. [Enlace](https://github.com/Azure/Azure-Data-Factory)
+        - **Azure SQL Database**: Almacenamiento estructurado de los datos.
+        - **Azure App Services**: Dashboard para visualizar los datos almacenados cada semana. [Enlace](https://github.com/Azure/App-Service)
+
+        Cabe destacar que las clasificaciones y etiquetas de localización (barrios, comunas) pueden variar en formato, debido a errores en los
+        metadatos proporcionados por los creadores de los anuncios. Para más detalles sobre los datos recopilados, puedes contactar al administrador.
+        """,
+        'fig_titles': {
+            'fig_col': 'Precio promedio (COP) por comuna (Medellín)',
+            'fig_pie': 'Cantidad de anuncios por municipio',
+            'fig_col2': 'Precio promedio (COP) por estrato',
+            'fig_line': 'Evolución temporal del precio promedio (COP)'
+        }
+    },
+    'en': {
+        'title': 'Housing Rental Price Monitor of the Aburrá Valley',
+        'sidebar_title': 'Technical Data',
+        'description': """
+        This project aims to provide reference data to analyze the dynamics of the real estate market in the region using web scraping techniques
+        from real estate listings websites. The goal is to identify the behavior of rental supply and prices in the Aburrá Valley. The collected
+        data corresponds to listings for apartments with at least two bedrooms and two bathrooms, using an automated process that employs free
+        Azure services.
+
+        The services used in this project are:
+
+        - **Azure Functions**: Automated web scraping. [Link](https://github.com/Azure/azure-functions)
+        - **Azure DataLake Gen2**: Storage of data collected via web scraping.
+        - **Azure Data Factory**: Ingestion of raw data into a SQL database. [Link](https://github.com/Azure/Azure-Data-Factory)
+        - **Azure SQL Database**: Structured storage of the data.
+        - **Azure App Services**: Dashboard for visualizing the stored data on a weekly basis. [Link](https://github.com/Azure/App-Service)
+
+        Please note that classifications and location labels (neighborhoods, communes) may vary in format due to errors in the metadata provided
+        by the listing creators. For more information about the data, you can contact the administrator.
+        """,
+        'fig_titles': {
+            'fig_col': 'Average Price (COP) by Commune (Medellín)',
+            'fig_pie': 'Number of Listings by Municipality',
+            'fig_col2': 'Average Price (COP) by Stratum',
+            'fig_line': 'Temporal Evolution of Average Price (COP)'
+        }
+    }
+}
+
+#Query con las vistas de la db
+df_stratpr, df_citypr, df_commpr, df_tlpr = sql_queries()
+
+def create_figures(titles):
+    def style_figure(fig, title):
+        fig.update_layout(
+            title={'font': {'size': 24, 'family': 'Arial', 'color': 'black'}, 'x': 0.5, 'xanchor': 'center'},
+            yaxis_tickformat=','
+        )
+        # fig.update_traces(hovertemplate='%{y:,.0f}')
+        return fig
+
+    fig_col = style_figure(px.bar(df_commpr, x="Comuna", y="PrecioPromedio", title=titles['fig_col']), titles['fig_col'])
+
+    fig_pie = px.pie(df_citypr, values="NumeroDeAnuncios", names="Ciudad", title=titles['fig_pie'])
+    fig_pie.update_layout(title={'font': {'size': 24, 'family': 'Arial', 'color': 'black'}, 'x': 0.5, 'xanchor': 'center'})
+
+    fig_pie.update_layout(showlegend=True)
+    fig_pie = style_figure(fig_pie, titles['fig_pie'])
+
+    fig_col2 = style_figure(px.bar(df_stratpr, x="Estrato", y="PrecioPromedio", title=titles['fig_col2']), titles['fig_col2'])
+    fig_line = style_figure(px.line(df_tlpr, x="Fecha", y="PrecioPromedio", title=titles['fig_line']), titles['fig_line'])
+
+    return fig_col, fig_pie, fig_col2, fig_line
+
+
+
+# Layout
+dash_app.layout = html.Div([
+    html.Div([
+        html.A(
+            [html.Span('🇨🇴', style={'font-size': '24px'}), ' Español'],
+            href='#', id='lang-es', style={'margin-right': '10px', 'text-decoration': 'none', 'color': 'black'}
+        ),
+        html.A(
+            [html.Span('🇬🇧', style={'font-size': '24px'}), ' English'],
+            href='#', id='lang-en', style={'text-decoration': 'none', 'color': 'black'}
+        )
+    ], style=LANGUAGE_BAR_STYLE),
+    
+    html.Div(id='sidebar', style=SIDEBAR_STYLE),
+    html.Div(id='content', style=CONTENT_STYLE),
+])
+
+
+
+
+@dash_app.callback(
+    [Output('sidebar', 'children'),
+     Output('content', 'children')],
+    [Input('lang-es', 'n_clicks'),
+     Input('lang-en', 'n_clicks')]
+)
+def update_language(lang_es, lang_en):
+    ctx = callback_context
+    if not ctx.triggered:
+        button_id = 'lang-es'
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    texts = content_text['es'] if button_id == 'lang-es' else content_text['en']
+    
+    fig_col, fig_pie, fig_col2, fig_line = create_figures(texts['fig_titles'])
+    
+    sidebar_content = html.Div([
+        html.H2(texts['sidebar_title'], className="display-4", style={'font-size': '25px', 'font-weight': 'bold'}),
+        html.Hr(),
+        dcc.Markdown(texts['description'], dangerously_allow_html=True)
+    ], style=SIDEBAR_STYLE)
+
+    content = html.Div([
+        html.H1(texts['title'], style={'textAlign': 'center', 'fontSize': '48px', 'margin-bottom': '30px'}),
+        
+        dbc.Row(dbc.Col(dcc.Graph(figure=fig_col), width=12)),
+        html.Br(),
+        dbc.Row([
+            dbc.Col(dcc.Graph(figure=fig_pie), width=6),
+            dbc.Col(dcc.Graph(figure=fig_col2), width=6),
+        ]),
+        html.Br(),
+        dbc.Row(dbc.Col(dcc.Graph(figure=fig_line), width=12))
+    ], style=CONTENT_STYLE)
+
+    return sidebar_content, content
+
+if __name__ == '__main__':
+    dash_app.run_server(debug=False)
---
+++ sql_utilities.py
@@ -0,0 +1,58 @@
+import pyodbc, os
+import pandas as pd
+# import warnings
+import struct
+from azure.identity import DefaultAzureCredential
+
+# warnings.filterwarnings('ignore')
+
+def sql_queries():
+    connection = get_conn()
+
+    if connection is None:
+        print("No se pudo establecer la conexión a la base de datos.")
+        return None, None, None, None
+    
+    try:
+        sql_query_stratpr = """
+        SELECT * FROM SCRP.StratumPrices
+        """ 
+        df_stratpr = pd.read_sql(sql_query_stratpr, connection) 
+        
+        sql_query_citypr = """
+        SELECT * FROM SCRP.CityPrices
+        """ 
+        df_citypr = pd.read_sql(sql_query_citypr, connection)
+        
+        sql_query_commpr = """
+        SELECT * FROM SCRP.MedCommunePrices
+        """ 
+        df_commpr = pd.read_sql(sql_query_commpr, connection)
+        
+        sql_query_tlpr = """
+        SELECT * FROM SCRP.PricesTL
+        """ 
+        df_tlpr = pd.read_sql(sql_query_tlpr, connection)
+        
+    except pyodbc.Error as e:
+        print(f"Error al ejecutar las queries: {e}")
+        return None, None, None, None
+    finally:
+        connection.close()
+    return df_stratpr, df_citypr, df_commpr, df_tlpr
+
+def get_conn(): #MS Learn function
+    try:
+        connection_string = os.environ["SQLAZURECONNSTR_ConMonitor"]
+        credential = DefaultAzureCredential(exclude_interactive_browser_credential=False)
+        token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
+        token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
+        
+        SQL_COPT_SS_ACCESS_TOKEN = 1256  # MS Defined
+        
+        conn = pyodbc.connect(connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
+        return conn
+
+    except pyodbc.Error as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
